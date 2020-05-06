a828eeb46196e05f1b71020128004ccfc73d5d7e
Annalise Murphy <nali247@gmail.com>
2020-05-06 11:46:58 -0700

    all pushed to dev branch

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (request, response) => {
+app.get('/location?:id', (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
     response.json({ hello: 'world' });
 });
+
+app.get('/weather', (request, response) => {
+    console.log('you hit the weather route');
+    response.json({ hello: 'weather' });
+});
+
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
